c9da30244c799817de7a4a86bcd7b10297754eea
aldairarchez <89089224+aldairarchez@users.noreply.github.com>
2024-02-09 09:15:59 -0600

    Create filter_clinvar.py

---
+++ Pipeline_Dementia/Codes/filter_clinvar.py
@@ -0,0 +1,18 @@
+import sys
+import pandas as pd
+
+# Obtener el valor de "gene" del argumento de línea de comandos
+gene = sys.argv[1]
+
+# Cargar el archivo CSV en un DataFrame
+data = pd.read_csv(f'{gene}_final.tsv', sep='\t', encoding='utf-8')
+
+pattern = r"(Pathogenic|Likely_pathogenic)"
+
+# Utilizar str.extract para obtener grupos de coincidencia en lugar de str.contains
+filtered_data = data[data['CLNSIG_2'].str.extract(pattern, expand=False).notna()] #CLNSIG_2 es la columna mas actualizada de la significancia en clinvar
+
+# Guardar los datos filtrados en un nuevo archivo
+filtered_data.to_csv(f'{gene}_inclinvar.tsv', sep='\t', index=False)
+filter_clinvar.py 
+
