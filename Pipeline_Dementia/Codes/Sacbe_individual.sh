4912197ca57ded3a8b76e8b6654afbf8d8f3c11e
aldairarchez <89089224+aldairarchez@users.noreply.github.com>
2024-02-09 01:09:22 -0600

    Update Sacbe_individual.sh

--- Pipeline_Dementia/Codes/Sacbe_individual.sh
+++ Pipeline_Dementia/Codes/Sacbe_individual.sh
@@ -13,7 +13,5 @@ tail -n +2 "$archivo" | while IFS=',' read -r GENE CHR START END; do
     archivo_salida="/mnt/Timina/cgonzaga/marciniega/Dementia_2024/genes_files/${GENE}_sacbe.tsv"
     #Obtaning the header
     awk NR==1 Marlon_sacbe.varfile.ex.EDITED > "$archivo_salida"
+    awk '$18 == "$GENE"' "$archivo_entrada" >> "$archivo_salida"
 
-    # Filtra las líneas del archivo de entrada según el intervalo deseado considerando la posicion 
-    awk -v inicio="$inicio" -v fin="$fin" '$2 ~ /^[0-9]+$/ && $2 >= inicio && $2 <= fin' "$archivo_entrada" >> "$archivo_salida"
-done
