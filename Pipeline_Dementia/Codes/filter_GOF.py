0f93314969324a994cb5a433d8f23c8b597608f0
aldairarchez <89089224+aldairarchez@users.noreply.github.com>
2024-02-29 11:34:02 -0600

    Update filter_GOF.py

--- Pipeline_Dementia/Codes/filter_GOF.py
+++ Pipeline_Dementia/Codes/filter_GOF.py
@@ -5,7 +5,7 @@ import pandas as pd
 gene = sys.argv[1]
 
 # Cargar el archivo CSV en un DataFrame
-data = pd.read_csv('/mnt/Timina/cgonzaga/marciniega/Dementia_2024/genes_files/'+f'{gene}_inclinvar1.csv', sep='\t', encoding='utf-8')
+data = pd.read_csv('/mnt/Timina/cgonzaga/marciniega/Dementia_2024/genes_files/'+f'{gene}_inclinvar1.tsv', sep='\t', encoding='utf-8')
 
 pattern = r"nonsynonymous SNV"
 
