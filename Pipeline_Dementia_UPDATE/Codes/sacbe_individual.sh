95a576f3df67be0e2f13ac9de1ddebc6d9357655
aldairarchez <89089224+aldairarchez@users.noreply.github.com>
2024-06-11 20:21:54 -0600

    Create sacbe_individual.sh

---
+++ Pipeline_Dementia_UPDATE/Codes/sacbe_individual.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Creates directory to save individual files
+mkdir sacbe_genes
+archivo="Genes.csv"
+# Obtener el encabezado fuera del bucle
+encabezado=$(awk NR==1 Marlon_sacbe.varfile.ex.EDITED)
+# Itera a través de cada línea en el archivo (omitir la primera línea con sed)
+tail -n +2 "$archivo" | while IFS=',' read -r GENE CHR START END; do
+    chrom=$(echo "$CHR" | tr -cd '[:alnum:]')
+    archivo_entrada="/mnt/Timina/cgonzaga/marciniega/Dementia_MCPS/Marlon_sacbe.varfile.ex.EDITED"
+    archivo_salida="/mnt/Timina/cgonzaga/marciniega/Dementia_MCPS/sacbe_genes/${GENE}_sacbe.tsv"
+    # Filtrar y agregar líneas al archivo de salida
+    echo "$encabezado" > "$archivo_salida"
+    #busca coincidecias PARCIALES (PSEN1,PSEN1)
+    awk -F'\t' -v GENE="$GENE" '$18 ~ GENE' "$archivo_entrada" >> "$archivo_salida"
+done
