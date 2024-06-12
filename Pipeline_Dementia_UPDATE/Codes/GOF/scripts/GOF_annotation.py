3b3e28175998a3846d4cbf9a22b3e4b7ff9ad755
aldairarchez <89089224+aldairarchez@users.noreply.github.com>
2024-06-11 20:42:31 -0600

    Create GOF_annotation.py

---
+++ Pipeline_Dementia_UPDATE/Codes/GOF/scripts/GOF_annotation.py
@@ -0,0 +1,30 @@
+import sys
+import pandas as pd
+
+# Obtener el valor de "gene" del argumento de línea de comandos
+#gene = sys.argv[1]
+# Leer la lista de genes desde LOF_genes.txt
+with open('GOF_genes.txt', 'r') as file:
+        gene_list = file.read().splitlines()
+
+# Iterar sobre cada gen en la lista
+for gene in gene_list:
+    # Cargar el archivo TSV en un DataFrame
+    input_file = f'/mnt/Timina/cgonzaga/marciniega/Dementia_MCPS/sacbe_genes/{gene}_sacbe.tsv'
+    data = pd.read_csv(input_file, sep='\t', encoding='utf-8')
+
+    # Lista de palabras clave exactas
+    keywords = ["nonsynonymous SNV"]
+
+    # Función para verificar si una celda contiene alguna palabra clave exacta
+    def contains_exact_keyword(cell, keywords):
+        return any(cell == keyword for keyword in keywords)
+
+    # Filtrar las filas que contienen alguna palabra clave exacta en alguna celda
+    filtered_data = data[data.apply(lambda row: any(contains_exact_keyword(str(value), keywords) for value in row), axis=1)]
+
+    # Especificar el archivo de salida
+    output_file = f'/mnt/Timina/cgonzaga/marciniega/Dementia_MCPS/GOF_files/{gene}_GOF_annotation.tsv'
+
+    # Guardar el DataFrame filtrado como un archivo TSV
+    filtered_data.to_csv(output_file, sep='\t', index=False, encoding='utf-8')
