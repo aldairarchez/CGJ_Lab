b79bce8e9e43298f6175ab3f961da6bb7237da39
aldairarchez <89089224+aldairarchez@users.noreply.github.com>
2024-08-14 16:51:00 -0600

    Update LOF_pipeline.sh

--- Pipeline_Dementia_UPDATE/Codes/LOF/LOF_pipeline.sh
+++ Pipeline_Dementia_UPDATE/Codes/LOF/LOF_pipeline.sh
@@ -2,7 +2,7 @@
 mkdir LOF_files
 #cargamos el modulo de python
 module load python38/3.8.3
-#corremos el script que filtra la columna annotations para quedarnos con variantes de GOF
+#corremos el script que filtra la columna annotations para quedarnos con variantes de LOF
 python3 LOF_annotation.py
 #we run the script that merges clinvar with MCPS and filter by pathogenicity
 python3 LOF_inclinvar.py
